327b0dec1be701d5b19d712c929650fdd6b539e8
Astisek <kamenskyh-is@itrev.ru>
2023-01-14 23:22:58 +0500

    Added eslint

--- actions/Leave/index.ts
+++ actions/Leave/index.ts
@@ -1,16 +1,15 @@
-import { SubscribeEnum } from "../../interfaces/BotMiddleware";
-import EmptyCommand from "../../models/EmptyCommand";
-import BotMiddleware from "../../service/BotMiddleware";
+import { SubscribeEnum } from '../../interfaces/BotMiddleware';
+import EmptyCommand from '../../models/EmptyCommand';
+import BotMiddleware from '../../service/BotMiddleware';
 
 class Leave extends EmptyCommand {
-  public execute = async () => { 
-    this.voiceConnection?.destroy()
-    // BotMiddleware.EmitEvent(SubscribeEnum.connectionStatus, {
-    //   channelId: this.channel.id,
-    //   connected: true
-    // })
-    this.logger('Leaved')
-  }
+  public execute = async () => {
+    this.voiceConnection?.destroy();
+    BotMiddleware.EmitEvent(SubscribeEnum.connectionStatus, this.channel.id, {
+      connected: false,
+    });
+    this.logger('Leaved');
+  };
 }
 
-export default Leave
+export default Leave;
